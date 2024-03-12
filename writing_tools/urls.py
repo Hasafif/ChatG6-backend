760bcb4573e2b4915f8ff088e136edb4602b7f64
Hasafif <147178688+Hasafif@users.noreply.github.com>
2024-03-12 08:03:53 -0400

    Update urls.py

--- writing_tools/urls.py
+++ writing_tools/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('literature/',views.LiteratureView.as_view(),name='literature'),
     path('documentation/',views.Documentation.as_view(),name='documentation'),
     path('plagiarism/',views.Plagiarism_detector.as_view(),name='plagiarism_detector'),
-    path('article/',views.Article.as_view(),name='article_generator')
+    path('article/',views.Article.as_view(),name='article_generator'),
+    path('outline/',views.Outline.as_view(),name='outline_generator')
 ]
